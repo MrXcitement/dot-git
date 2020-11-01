1de6b6d039a69368394393eb2730147b593d4147
Mike Barker <mike@thebarkers.com>
2020-11-01 08:16:11 -0700

    Issue #5: Test global gitconfig for errors (#8)
    * Issue #5: Test global gitconfig for errors
    
    Run git config to test that the `.gitconfig` file does not have any syntax errors
    Renamed tests to run_tests.sh
    Changed shellcheck to check `run_tests.sh` not `tests`
    Changed header comment to reflect renamed script from `tests` to `run_tests.sh`

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# run_tests.sh --- run static tests against this repository
+# Mike Barker <mike@thebarkers.com>
+# March 14th 2019
+
+shellcheck {install,run_tests.sh}
+
+# Listing the configuration will fail if there are format errors in the gitconfig file
+# It does not check for valid settings, just valid formating.
+git --no-pager config -f ./home/.gitconfig -l > /dev/null
+
